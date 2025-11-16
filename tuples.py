4c617eabd7ca6997e55a4df90eb86476f237c2f9
Ashwanth Fernando <ashwanthfernando@gmail.com>
2025-11-16 11:45:07 +1100

    Changed files: - tuples.py

---
+++ tuples.py
@@ -0,0 +1,9 @@
+day_pair = ("Monday", 1)
+
+name_age_and_sex = ("Ash", 20, "Male")
+
+fruits = [('banana', 'yellow', 10), ('apple', 'red', 19)]
+
+# Destructuring/Unpacking the tuple with a for loop
+for name, color, price in fruits:
+    print(name, color, price)
