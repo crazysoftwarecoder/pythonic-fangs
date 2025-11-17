829849aed5a866a57df1fdb4f2eb72ab5542d3b1
Ashwanth Fernando <ashwanthfernando@gmail.com>
2025-11-17 17:50:26 +1100

    Add numbers

--- tuples.py
+++ tuples.py
@@ -7,3 +7,12 @@ fruits = [('banana', 'yellow', 10), ('apple', 'red', 19)]
 # Destructuring/Unpacking the tuple with a for loop
 for name, color, price in fruits:
     print(name, color, price)
+
+# Unpacking arguments
+
+def add(a, b):
+    return a + b
+
+numbers = (1, 2)
+
+print(add(*numbers))
